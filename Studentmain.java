944bc77697dade9cc0bf22733ebe7d77fef7e3a1
KuhooSaxena <88498142+KuhooSaxena@users.noreply.github.com>
2021-11-13 11:26:02 +0530

    Add files via upload

---
+++ Studentmain.java
@@ -0,0 +1,28 @@
+class Student {
+    String name;
+
+    Student() {
+        name = "Unknown";
+    }
+
+    Student(String s) {
+        name = s;
+    }
+
+    public void show() {
+        System.out.println(name);
+    }
+}
+
+public class Studentmain
+{
+
+    public static void main(String[] args)
+    {
+        Student obj=new Student();
+        Student obj1=new Student("Abc");
+        obj.show();
+        obj1.show();
+    }
+
+}
